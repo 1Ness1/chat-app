bc80e196dffb1400e57504847d98482e7d605b97
Oleg Berlo <oleg.b@oxtech.org>
2024-04-19 20:34:45 +0300

    Added project setup

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import { config } from "dotenv";
+import authRoutes from "./routes/auth.routes.js";
+
 const app = express();
 
 config();
@@ -9,6 +11,20 @@ const PORT = process.env.PORT || 5000;
 app.get("/", (req, res) => {
     // root route http://localhost:5000/
     res.send("Hello worlds")
-})
+});
+
+app.use("/api/auth", authRoutes);
+
+// app.get("/api/auth/signup", (req, res) => {
+//     console.log("signup route")
+// });
+
+// app.get("/api/auth/login", (req, res) => {
+//     console.log("login route")
+// });
+
+// app.get("/api/auth/logout", (req, res) => {
+//     console.log("logout route")
+// });
 
 app.listen(PORT, () => console.log(`Server running on this ${PORT}`));
