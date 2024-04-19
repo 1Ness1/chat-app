0778d2729fa573a524f60e51b7cdca06c1205e2b
Oleg Berlo <oleg.b@oxtech.org>
2024-04-19 16:06:52 +0300

    Added project setup

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import { config } from "dotenv";
+const app = express();
+
+config();
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+    // root route http://localhost:5000/
+    res.send("Hello worlds")
+})
+
+app.listen(PORT, () => console.log(`Server running on this ${PORT}`));
