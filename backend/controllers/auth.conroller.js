12f7de6ce9927b53b6c85b6563b264755f1eef52
Oleg Berlo <oleg.b@oxtech.org>
2024-04-22 21:47:33 +0300

    Added endpoint to save user

--- backend/controllers/auth.conroller.js
+++ backend/controllers/auth.conroller.js
@@ -1,18 +1,53 @@
+import User from "../models/user.model.js";
+
 export const login = async (req, res) => {
-    try {
-        const {fullName, userName, password, configPassword, gender} = req.body;
-
-    } catch(error) {
-
-    }
-
     res.send("Login");
 };
 
-export const signup = (req, res) => {
+export const signup = async (req, res) => {
     console.log("Signup");
 
-    res.send("Signup");
+    try {
+        const {fullName, username, password, confirmPassword, gender} = req.body;
+        console.log(req.body)
+
+        if(password !== confirmPassword) {
+            return res.status(400).json({error: `Password don't match`})
+        }
+
+        const user = await User.findOne({username});
+
+        if(user) {
+            return res.status(400).json({error: `This user exists`});
+        }
+
+        // HASH PASSWORD
+
+        const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+        const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+        const newUser = new User({
+            fullName,
+            username,
+            password,
+            gender,
+            profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
+        })
+
+        await newUser.save();
+
+        res.status(201).json({
+            _id: newUser._id,
+            fullName: newUser.fullName,
+            username: newUser.username,
+            gender: newUser.gender,
+            profilePic: newUser.profilePic
+        })
+    } catch(error) {
+        console.error(`Error in signup controller: ${error.message}`);
+        res.status(500).json({error: "Internal Server Error"});
+    }
+
 };
 
 export const logout = (req, res) => {
--- backend/server.js
+++ backend/server.js
@@ -4,17 +4,18 @@ import authRoutes from "./routes/auth.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 config();
 
-const PORT = process.env.PORT || 5000;
-
-app.get("/", (req, res) => {
-    // root route http://localhost:5000/
-    res.send("Hello worlds")
-});
-
+app.use(express.json()); // to parse incomings requests with JSON payloads (from req.body)
 app.use("/api/auth", authRoutes);
+
+// app.get("/", (req, res) => {
+//     root route http://localhost:5000/
+//     res.send("Hello worlds")
+// });
+
 
 app.listen(PORT, () => {
     connectToMongoDB();
