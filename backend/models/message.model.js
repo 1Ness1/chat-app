251e365368cb77cee4a4bff17a677597e97f935b
Oleg B <oleg.b@oxtech.org>
2024-04-24 13:55:55 +0300

    Added conversation|message model

---
+++ backend/models/message.model.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema({
+    senderId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        require: true,
+    },
+
+    receiverId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        require: true,
+    },
+
+    message: {
+        type: String,
+        require: true
+    }
+    // createdAt, updatedAt => message.createdAt : 15:30
+
+
+}, {timestamps: true});
+
+const Message = mongoose.model("Message", messageSchema)
+
+export default Message;
