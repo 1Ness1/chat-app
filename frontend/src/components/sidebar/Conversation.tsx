ab4b7a16ea95b763896f9ef5697eb201b8c99791
Oleg B <oleg.b@oxtech.org>
2024-05-02 20:22:18 +0300

    Added handle send message

--- frontend/src/components/sidebar/Conversation.tsx
+++ frontend/src/components/sidebar/Conversation.tsx
@@ -1,21 +1,25 @@
-const Conversation = () => {
+import useConversation from "../../store/useConversation";
+
+const Conversation = ({conversation, emoji, lastIndex}) => {
+    const {selectedConversation, setSelectedConversation} = useConversation();
+    const isSelected = selectedConversation?._id === conversation._id;
   return (
     <>
-        <div className="flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer">
+        <div className={`flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer ${isSelected ? "bg-sky-500" : ""}`} onClick={() => setSelectedConversation(conversation)}>
             <div className="avatar online">
                 <div className="w-12 rounded-full">
-                    <img src="https://avatar.iran.liara.run/public/boy?username=%22Ness%22" alt="user avatar"></img>
+                    <img src={conversation.profilePic} alt="user avatar"></img>
                 </div>
             </div>
 
             <div className="flex flex-col flex-1">
                 <div className="flex gap-3 justify-between">
-                    <p className="font-bold text-gray-200">John Doe</p>
-                    <span className="text-xl">😱</span>
+                    <p className="font-bold text-gray-200">{conversation.fullName}</p>
+                    <span className="text-xl">{emoji}</span>
                 </div>
             </div>
         </div>
-        <div className="divider my-0 py-0 h-1" />
+        {!lastIndex && <div className="divider my-0 py-0 h-1" />}
     </>
   )
 }
--- frontend/src/components/sidebar/Conversations.tsx
+++ frontend/src/components/sidebar/Conversations.tsx
@@ -1,13 +1,20 @@
+import useGetConversations from "../../hooks/useGetConversations"
 import Conversation from "./Conversation"
-
+import { getRandomEmoji } from "../../utils/emoji";
 const Conversations = () => {
+  const {loading, conversations} = useGetConversations();
+
   return (
-    <div className="py-2 flex flex-col overflow-auto">
-        <Conversation />
-        <Conversation />
-        <Conversation />
-        <Conversation />
-        <Conversation />
+    <div className={`py-2 flex flex-col overflow-auto `}>
+      {conversations.map((conversation, index) => (
+        <Conversation key={conversation._id}
+          conversation={conversation}
+          emoji={getRandomEmoji()}
+          lastIndex={index === conversations.length - 1}
+        />
+      ))}
+
+       {loading ? <span className="loading loading-spinner"></span> : null}
     </div>
   )
 }
